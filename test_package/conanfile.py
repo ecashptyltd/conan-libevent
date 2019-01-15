d33f00d7e47947db54982b9e96ad22f73113b4fb
Michael Beach <michaelb@ieee.org>
2019-01-15 16:12:45 +1100

    Don't attempt to run the test package when cross-building.

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, CMake
+from conans import ConanFile, CMake, tools
 import os
 
 
@@ -15,5 +15,8 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        bin_path = os.path.join("bin", "test_package")
-        self.run(bin_path, run_environment=True)
+        if tools.cross_building(self.settings):
+            self.output.warn("Skipping run of cross-built package")
+        else:
+            bin_path = os.path.join("bin", "test_package")
+            self.run(bin_path, run_environment=True)
